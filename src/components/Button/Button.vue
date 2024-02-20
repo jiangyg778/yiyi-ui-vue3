5662a4005e99bfb40897c401ada221196d07e135
DESKTOP-DO9B8F8\admin <297138663@qq.com>
2024-02-20 22:44:13 +0800

    feat: button

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Author: DESKTOP-DO9B8F8\admin 297138663@qq.com
+ * @Date: 2024-02-20 21:55:35
+ * @LastEditors: DESKTOP-DO9B8F8\admin 297138663@qq.com
+ * @LastEditTime: 2024-02-20 22:30:04
+ * @FilePath: \yiyi-ui-vue3\src\components\Button\Button.vue
+ * @Description: button组件
+-->
+<!--  -->
+<template>
+  <button
+    class="yy-button"
+    :class="{
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      [`yy-button--${type}`]: type,
+      [`yy-button--${size}`]: size
+    }"
+    :disabled="disabled"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ButtonProps } from './types'
+defineProps<ButtonProps>()
+</script>
---
+++ src/components/Button/types.ts
@@ -0,0 +1,41 @@
+/*
+ * @Author: DESKTOP-DO9B8F8\admin 297138663@qq.com
+ * @Date: 2024-02-20 21:53:02
+ * @LastEditors: DESKTOP-DO9B8F8\admin 297138663@qq.com
+ * @LastEditTime: 2024-02-20 22:20:29
+ * @FilePath: \yiyi-ui-vue3\src\components\Button\types.ts
+ * @Description: 
+ */
+import {PropType} from 'vue'
+
+export type ButtonType = 'primary' |'success'| 'warning' | 'danger' | 'info' 
+export type ButtonSize = 'large' | 'medium' | 'small' | 'mini'
+
+export interface ButtonProps {
+  type?: ButtonType
+  size?: ButtonSize
+ plain?: boolean
+  round?: boolean
+  circle?: boolean
+  disabled?: boolean
+}
+export const buttonProps = {
+  type: {
+    type: String as PropType<ButtonProps>
+  },
+  size: {
+    type: String as PropType<ButtonSize>
+  },
+  plain: {
+    type: Boolean as PropType<boolean>
+  },
+  round: {
+    type: Boolean as PropType<boolean>
+  },
+  circle: {
+    type: Boolean as PropType<boolean>
+  },
+  disabled: {
+    type: Boolean as PropType<boolean>
+  }
+}
